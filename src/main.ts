109f52460066df20687a0194f22c7764fa3c8e17
Jose Alberto Delgado Robles <jdelgadorobles84@gmail.com>
2024-09-19 13:05:34 +0200

    feat: initial commit to github organization

---
+++ src/main.ts
@@ -0,0 +1,32 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { Logger, ValidationPipe } from '@nestjs/common';
+import { envs } from './config';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
+
+async function bootstrap() {
+  const app = await NestFactory.createMicroservice<MicroserviceOptions>(
+    AppModule,
+    {
+      transport: Transport.TCP,
+      options: {
+        port: envs.port,
+      },
+    },
+  );
+  const logger = new Logger('ProductsMS-main');
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+    }),
+  );
+
+  await app.listen();
+
+  // logger.log(`Application is running on: ${await app.getUrl()}`);
+  // logger.log(`Application is running on: ${envs.port}`);
+  logger.log(`Products microservice is running on: ${envs.port}`);
+}
+bootstrap();
---
+++ src/products/dto/create-product.dto.ts
@@ -0,0 +1,24 @@
+import { Type } from 'class-transformer';
+import { IsNumber, IsOptional, IsString, Min } from 'class-validator';
+
+export class CreateProductDto {
+  @IsString()
+  name: string;
+
+  @IsString()
+  @IsOptional()
+  description?: string;
+
+  @IsNumber({
+    maxDecimalPlaces: 4,
+  })
+  @Min(0)
+  @Type(() => Number)
+  price: number;
+
+  @IsNumber()
+  @IsOptional()
+  @Min(0)
+  @Type(() => Number)
+  stock?: number;
+}
