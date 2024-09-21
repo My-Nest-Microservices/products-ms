a95aeb09e37f0e24d0d8331dbcb403447e1c0a68
Jose Alberto Delgado Robles <jdelgadorobles84@gmail.com>
2024-09-21 12:09:02 +0200

    fix: problem with dtos

--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -1,20 +1,9 @@
-import {
-  Controller,
-  // Get,
-  // Post,
-  // Body,
-  // Patch,
-  // Param,
-  // Delete,
-  // Query,
-  // ParseUUIDPipe,
-  ParseIntPipe,
-} from '@nestjs/common';
+import { Controller, ParseIntPipe } from '@nestjs/common';
 import { ProductsService } from './products.service';
-import { CreateProductDto } from './dto/create-product.dto';
-import { UpdateProductDto } from './dto/update-product.dto';
 import { PaginationDto } from 'src/common/dto';
 import { MessagePattern, Payload } from '@nestjs/microservices';
+import { CreateProductDto } from './dto/create-product.dto';
+import { UpdateProductDto } from './dto/update-product.dto';
 
 @Controller('products')
 export class ProductsController {
